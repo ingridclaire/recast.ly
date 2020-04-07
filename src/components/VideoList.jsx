947af7b285f9522d9b5cce0c883012873a87a839
Carly Larko <carlynichole09@gmail.com>
2020-04-07 15:43:27 -0400

    implement for in loop to dynamically add data from data object

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,16 @@
-import exampleVideoData from
-'../data/exampleVideoData.js';
- console.log(exampleVideoData)
+import exampleVideoData from '../data/exampleVideoData.js';
 var VideoList = () => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {/* videoListEntry snippet.title snippet.description  - title and description   video goes here -- vidoeURL - thumbnails.default.url */}
+    {/* return (
+   {for (var entry of exampleVideoData) {
+      var title = entry.snippet.title;
+      var description = entry.snippet.description;
+      var videoLink = entry.snippet.thumbnails.default.url;
+    <div><h5><em>{`${title}; ${description}`}</em>{videLink}</h5></div>
+  }}) */}
   </div>
+
 );
 
 // PropTypes tell other developers what `props` a component expects
