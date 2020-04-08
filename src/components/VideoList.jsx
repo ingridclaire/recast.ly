6f24b77fb70090e2fb8ae3b2b0ad0ae83cc3dca2
Carly Larko <carlynichole09@gmail.com>
2020-04-08 00:18:57 -0400

    render videoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,24 @@
-import exampleVideoData from '../data/exampleVideoData.js';
-var VideoList = () => (
-  <div className="video-list">
-    {/* videoListEntry snippet.title snippet.description  - title and description   video goes here -- vidoeURL - thumbnails.default.url */}
-    {/* return (
-   {for (var entry of exampleVideoData) {
-      var title = entry.snippet.title;
-      var description = entry.snippet.description;
-      var videoLink = entry.snippet.thumbnails.default.url;
-    <div><h5><em>{`${title}; ${description}`}</em>{videLink}</h5></div>
-  }}) */}
-  </div>
 
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
+
+    <div className="video-list">
+
+    <div>
+      {props.videos.map((video) =>
+        <VideoListEntry video={video} id={video.id.videoId}/>
+      )}
+    </div>
+  </div>
+  // console.log(props.videos)
+  //   return (
+  //   <div className="video-list">
+  // {/* //   {exampleVideoData.map(entry => ( */}
+  // // <div><h5><em>key={entry.snippet.title}{entry.snippet.description}</em>{entry.snippet.thumbnails.default.url}</h5></div>
+  // //    ))}
+  //   </div>
+  // )
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -21,4 +29,14 @@ VideoList.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
+
+// ReactDOM.render(<VideoList, document.getElementByClassName("video-list")/>);
 export default VideoList;
+
+
+// {for (var entry of exampleVideoData) {
+//   var title = entry.snippet.title;
+//   var description = entry.snippet.description;
+//   var videoLink = entry.snippet.thumbnails.default.url;
+// <div><h5><em>{`${title}; ${description}`}</em>{videLink}</h5></div>
+// }})
